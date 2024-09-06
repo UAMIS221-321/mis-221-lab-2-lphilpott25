7d80667178ecccfcca0ff3bdd08035e070024b13
Liam Philpott <lphilpott@crimson.ua.edu>
2024-09-06 14:04:42 -0500

    version 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿//know how to use %
+//constants are all caps
+
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.1;
+
+int numberOfSandwiches = 0;
+int numberOfToppings = 0;
+double tip = 0;
+
+System.Console.WriteLine("How many sandwich do you want?");
+numberOfSandwiches = int.Parse(Console.ReadLine()); 
+
+System.Console.WriteLine("enter nember of toppings");
+tip = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter tip amount");
+tip = double.Parse(Console.ReadLine());
+
+double totalSandWichCOst = COST_OF_SANDWICH * numberOfSandwiches;
+double totalToppingCost = COST_OF_TOPPING * numberOfToppings;
+double baseCost = totalSandWichCOst + totalToppingCost;
+
+double orderCost = tip + baseCost*(1-DISCOUNT_AMOUNT);
+System.Console.WriteLine("Your total Cost is " + orderCost);
+
+
--- obj/Debug/net8.0/mis-221-lab-2-lphilpott25.AssemblyInfo.cs
+++ obj/Debug/net8.0/mis-221-lab-2-lphilpott25.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mis-221-lab-2-lphilpott25")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6beb5fcdfcf434119a0b4b5205353d107cac576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7827b1fd4c788ad1c02b717e63079bf71ab1de")]
 [assembly: System.Reflection.AssemblyProductAttribute("mis-221-lab-2-lphilpott25")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mis-221-lab-2-lphilpott25")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
